b5ccab3bf24f8751f87bdf5222540041b95ffd3a
Michael <maw119@ic.ac.uk>
2023-10-03 11:04:07 +0100

    fixed deploy script

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -3,18 +3,16 @@ const fs = require("fs");
 
 (async () => {
   try {
-    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    // Switch to the existing 'gh-pages' branch
+    await execa("git", ["checkout", "gh-pages"]);
     console.log("Building...");
     await execa("npm", ["run", "build"]);
-    // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "Update gh-pages"]);
     console.log("Pushing to gh-pages...");
-    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await execa("git", ["push", "origin", "gh-pages", "--force"]);
     await execa("rm", ["-r", folderName]);
-    await execa("git", ["checkout", "-f", "master"]);
-    await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed");
   } catch (e) {
     console.log(e.message);
