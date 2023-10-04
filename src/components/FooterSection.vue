0defda1eb8bf3bf012d7fe354a6af14bd9b52c9d
Michael <maw119@ic.ac.uk>
2023-10-04 13:30:35 +0100

    new Footer

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,9 +1,14 @@
 <template>
     <div class="footer">
-        Michael Adhipramana Widjaya
+        <div class="footer-title">Michael Adhipramana Widjaya</div>
+        <div class="vertical-line">|</div>
         <a href="https://www.linkedin.com/in/michaelwidjaya/ " target="_blank">LinkedIn</a>
+        <div class="vertical-line">|</div>
         <a href="https://github.com/idkkk11" target="_blank"> GitHub </a>
+        <div class="vertical-line">|</div>
         <div id="to-top" @click="toTop">Return to Top</div>
+        <div class="vertical-line">|</div>
+        Developed on Vue.js
         
     </div>
 </template>
@@ -29,11 +34,35 @@ export default {
 .footer {
     display: flex;
     padding: 100px 0 5vh 0;
-    flex-direction: column;
+    flex-direction: row;
     justify-content: center;
     align-items: center;
     gap: 10px;
     color: #FFF;
     font-size: min(max(1.25vw, 12px),20px);
+}
+
+.footer-title {
+    font-weight: 800;
+}
+
+#to-top {
+    cursor: pointer;
+}
+
+@media (orientation: portrait) {
+    .footer {
+    display: flex;
+    flex-direction: column;
+    padding: 100px 0 5vh 0;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    color: #FFF;
+    font-size: min(max(1.25vw, 12px),20px);
+}
+.vertical-line {
+    display: none
+}
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router';
+
 import HomeView from '../views/HomeView.vue'
 
 
@@ -9,69 +11,114 @@ const routes = [
     name: 'home',
     component: HomeView
   },
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: HomeView
+  // },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: '/about',  // Update path to include #
+    name: 'about', // You can keep the name as is
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-    meta: {
-      title: 'Home Page - Example App',
-      metaTags: [
-        {
-          name: 'description',
-          content: 'The home page of our example app.'
-        },
-        {
-          property: 'og:description',
-          content: 'The home page of our example app.'
-        }
-      ]
-    }
+    // ...meta and other configurations...
   },
   {
-    path: '/va',
+    path: '/va', // Update path to include #
     name: 'voiceassistant',
     component: () => import(/* webpackChunkName: "about" */ '../views/projects/ProjectAlexa.vue')
   },
   {
-    path: '/3d',
+    path: '/3d', // Update path to include #
     name: '3dlabs',
     component: () => import(/* webpackChunkName: "about" */ '../views/projects/Project3D.vue')
   },
   {
-    path: '/emotech',
+    path: '/emotech', // Update path to include #
     name: 'emotech',
     component: () => import(/* webpackChunkName: "about" */ '../views/projects/ProjectEmotech.vue')
   },
   {
-    path: '/quad',
+    path: '/quad', // Update path to include #
     name: 'quad',
     component: () => import(/* webpackChunkName: "about" */ '../views/projects/ProjectQuad.vue')
   },
   {
-    path: '/lesku',
+    path: '/lesku', // Update path to include #
     name: 'lesku',
     component: () => import(/* webpackChunkName: "about" */ '../views/projects/ProjectLesku.vue')
   },
   {
-    path: '/philips',
+    path: '/philips', // Update path to include #
     name: 'philips',
     component: () => import(/* webpackChunkName: "about" */ '../views/projects/ProjectPhilips.vue')
   }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+    // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    // meta: {
+    //   title: 'Home Page - Example App',
+    //   metaTags: [
+    //     {
+    //       name: 'description',
+    //       content: 'The home page of our example app.'
+    //     },
+    //     {
+    //       property: 'og:description',
+    //       content: 'The home page of our example app.'
+  //       }
+  //     ]
+  //   }
+  // },
+  // {
+  //   path: '/va',
+  //   name: 'voiceassistant',
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/projects/ProjectAlexa.vue')
+  // },
+  // {
+  //   path: '/3d',
+  //   name: '3dlabs',
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/projects/Project3D.vue')
+  // },
+  // {
+  //   path: '/emotech',
+  //   name: 'emotech',
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/projects/ProjectEmotech.vue')
+  // },
+  // {
+  //   path: '/quad',
+  //   name: 'quad',
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/projects/ProjectQuad.vue')
+  // },
+  // {
+  //   path: '/lesku',
+  //   name: 'lesku',
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/projects/ProjectLesku.vue')
+  // },
+  // {
+  //   path: '/philips',
+  //   name: 'philips',
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/projects/ProjectPhilips.vue')
+  // }
 ]
 
 // const router = createRouter({
 //   history: createWebHistory(process.env.BASE_URL),
 //   routes,
-//   scrollBehavior() {
-//     document.getElementById('app').scrollIntoView({ behavior: 'smooth' });
-// }
-// })
+//   scrollBehavior(to, from, savedPosition) {
+//     if (savedPosition) {
+//       return savedPosition;
+//     } else {
+//       return { top: 0 };
+//     }
+//   },
+// });
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
